b62df499c8385df8f57927747f36f2624e882530
David <wisekrakr@gmail.com>
2019-09-16 05:22:08 +0200

    prettier engaged

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,50 +1,48 @@
 // Select DOM Items
-  
-    // Menu Items
-const menuButton = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item'); //puts all items in an array
-const siteMenu = document.querySelector('.site-menu');
 
-   
+// Menu Items
+const menuButton = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item"); //puts all items in an array
+const siteMenu = document.querySelector(".site-menu");
+
 // Set Initial State of Menu
 
 let showMenu = false; //let can be reassigned at any time
 
-menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener("click", toggleMenu);
 
-function toggleMenu(){
-    if(!showMenu){
-        menuButton.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show')); //each item in nav gets show function
+function toggleMenu() {
+  if (!showMenu) {
+    menuButton.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show")); //each item in nav gets show function
 
-        //Set Menu State
-        showMenu = true;
-    }else{
-        menuButton.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show')); //each item in nav gets show function
+    //Set Menu State
+    showMenu = true;
+  } else {
+    menuButton.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show")); //each item in nav gets show function
 
-        //Set Menu State
-        showMenu = false;
-    }
+    //Set Menu State
+    showMenu = false;
+  }
 }
 
 // Scrolling navbar. Navbar disappears at the bottom of the screen
 var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
-
   var currentScrollPos = window.pageYOffset;
 
-  if(document.getElementById("navbar") !== null){
+  if (document.getElementById("navbar") !== null) {
     if (prevScrollpos > currentScrollPos) {
       document.getElementById("navbar").style.bottom = "-55px";
     } else {
@@ -52,10 +50,8 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
       document.getElementById("navbar").style.bottom = "-55px";
     }
   }
-}
-
-
+};
--- dist/js/time.js
+++ dist/js/time.js
@@ -1,136 +1,163 @@
- // Select DOM Items
- 
-    // Time Items (Philosophy page)
- const time = document.getElementById('time');
- const greeting = document.getElementById('greeting');
- const name = document.getElementById('name');
- const thought = document.getElementById('thought');
- let is24Hours = true;
- let changeButton = document.getElementById("changeFormat");
- 
-// Show Time
-function updateTime(){ //Add button and function to set
-    let today = new Date();
-    let hour = today.getHours();
-    let minute = today.getMinutes();
-    let second = today.getSeconds();    
-    
-    // Set AM or PM
-    if(!is24Hours){
-      const amPm = hour >= 12 ? 'PM' : 'AM';
-  
-      // 12hr Format
-      let newHour = hour %12 || 12;  
+// Select DOM Items
 
-      time.innerHTML = `${newHour}<span>:</span>${addZero(minute)}<span>:</span>${addZero(second) + amPm}`;
-    }else{
-      time.innerHTML = `${hour}<span>:</span>${addZero(minute)}<span>:</span>${addZero(second)}`;
-    
-    }
-    
-    setTimeout(updateTime,1000);
+// Time Items (Philosophy page)
+const time = document.getElementById("time");
+const greeting = document.getElementById("greeting");
+const name = document.getElementById("name");
+const thought = document.getElementById("thought");
+let is24Hours = true;
+let changeButton = document.getElementById("changeFormat");
+
+// Show Time
+function updateTime() {
+  //Add button and function to set
+  let today = new Date();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+
+  // Set AM or PM
+  if (!is24Hours) {
+    const amPm = hour >= 12 ? "PM" : "AM";
+
+    // 12hr Format
+    let newHour = hour % 12 || 12;
+
+    time.innerHTML = `${newHour}<span>:</span>${addZero(
+      minute
+    )}<span>:</span>${addZero(second) + amPm}`;
+  } else {
+    time.innerHTML = `${hour}<span>:</span>${addZero(
+      minute
+    )}<span>:</span>${addZero(second)}`;
+  }
+
+  setTimeout(updateTime, 1000);
 }
 
 // Change from 24 hours to 12 hours
 let changeFormat = function() {
   is24Hours = !is24Hours;
 };
-  
-  // Add Zeros to Time
-function addZero(n){
-    return(parseInt(n, 10) < 10 ? '0' : '') + n;
-} 
-  
-  // Set Background and Greeting
-function setGreeting(){
-    let today = new Date();
-    let hour = today.getHours();
 
-    switch(hour){
-      case 0: case 1: case 2: case 3: case 4: case 5:
-        //Night
-        document.body.style.backgroundImage = "url(https://raw.githubusercontent.com/wisekrakr/resources_only/master/night.jpg)";
-        greeting.textContent = "Good Night ";
-       
-        break;
-      case 6: case 7: case 8: case 9: case 10: case 11:
-        //Morning
-        document.body.style.backgroundImage = "url(https://raw.githubusercontent.com/wisekrakr/resources_only/master/morning.jpg)";
-        greeting.textContent = "Good Morning ";
-        
-        break;
-      case 12: case 13: case 14: case 15: case 16: case 17:
-        //Afternoon
-        document.body.style.backgroundImage = "url(https://raw.githubusercontent.com/wisekrakr/resources_only/master/afternoon.jpg)";
-        greeting.textContent = "Good Afternoon ";
-        
-        break;
-      case 18: case 19: case 20: case 21: case 22: case 23:
-        //Evening
-        document.body.style.backgroundImage = "url(https://raw.githubusercontent.com/wisekrakr/resources_only/master/evening.jpg)";
-        greeting.textContent = "Good Evening ";      
-
-        break;
-      default:
-          console.log("No Time Given " + hour);
-    }
+// Add Zeros to Time
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
 
-  //Get Name
-function getName(){
-  if(localStorage.getItem('name') === null){
-    name.innerHTML = "[Enter your name]";
-  }else{
-    name.textContent = localStorage.getItem('name');
+// Set Background and Greeting
+function setGreeting() {
+  let today = new Date();
+  let hour = today.getHours();
+
+  switch (hour) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      //Night
+      document.body.style.backgroundImage =
+        "url(https://raw.githubusercontent.com/wisekrakr/resources_only/master/night.jpg)";
+      greeting.textContent = "Good Night ";
+
+      break;
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+      //Morning
+      document.body.style.backgroundImage =
+        "url(https://raw.githubusercontent.com/wisekrakr/resources_only/master/morning.jpg)";
+      greeting.textContent = "Good Morning ";
+
+      break;
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+      //Afternoon
+      document.body.style.backgroundImage =
+        "url(https://raw.githubusercontent.com/wisekrakr/resources_only/master/afternoon.jpg)";
+      greeting.textContent = "Good Afternoon ";
+
+      break;
+    case 18:
+    case 19:
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+      //Evening
+      document.body.style.backgroundImage =
+        "url(https://raw.githubusercontent.com/wisekrakr/resources_only/master/evening.jpg)";
+      greeting.textContent = "Good Evening ";
+
+      break;
+    default:
+      console.log("No Time Given " + hour);
   }
 }
 
-  // Set Name
-function setName(event){
-  if(event.type === 'keypress'){
+//Get Name
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.innerHTML = "[Enter your name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+// Set Name
+function setName(event) {
+  if (event.type === "keypress") {
     // Check if Enter is pressed
 
-    if(event.keyCode === 13){ //13 is the Enter key
-      localStorage.setItem('name', event.target.innerText);
+    if (event.keyCode === 13) {
+      //13 is the Enter key
+      localStorage.setItem("name", event.target.innerText);
       name.blur();
     }
-
-  }else{
-    localStorage.setItem('name', event.target.innerText);
+  } else {
+    localStorage.setItem("name", event.target.innerText);
   }
 }
 
-  // Get Thought
-function getThought(){
-  if(localStorage.getItem('thought') === null){
+// Get Thought
+function getThought() {
+  if (localStorage.getItem("thought") === null) {
     thought.textContent = "Tell me your thoughts here";
-  }else{
-    thought.textContent = localStorage.getItem('thought');
+  } else {
+    thought.textContent = localStorage.getItem("thought");
   }
 }
 
-
-  // Set Thought
-function setThought(event){
-  if(event.type === 'keypress'){
+// Set Thought
+function setThought(event) {
+  if (event.type === "keypress") {
     // Check if Enter is pressed
 
-    if(event.keyCode === 13){ //13 is the Enter key
-      localStorage.setItem('thought', event.target.innerText);
+    if (event.keyCode === 13) {
+      //13 is the Enter key
+      localStorage.setItem("thought", event.target.innerText);
       thought.blur();
     }
-
-  }else{
-    localStorage.setItem('thought', event.target.innerText);
+  } else {
+    localStorage.setItem("thought", event.target.innerText);
   }
 }
-  
+
 // Event Listeners
 changeButton.addEventListener("click", changeFormat);
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
-thought.addEventListener('keypress', setThought);
-thought.addEventListener('blur', setThought);
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+thought.addEventListener("keypress", setThought);
+thought.addEventListener("blur", setThought);
 
 // Run Functions
 updateTime();
